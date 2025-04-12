1f4424cc06990f7dc1a96f69f90c84059e0aa7a0
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 04:49:17 +0530

    middleware

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
 
+const middlewareUser = (req, res, next)=>{
+    req.user = "Guest";
+    next();
+}
 
-app.get('/', (req, res)=>{
-    res.send("hello world");
+app.get('/welcome', middlewareUser, (req, res)=>{
+    res.send(`<h1>hello ${req.user}</h1>`);
 })
 
-app.listen(port, ()=>{
-    console.log("server is running")
-});
+app.listen(3000, ()=>{
+    console.log("server is running");
+})
