40ee3984df0ee2120bff49a4928ef979d48481db
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 05:12:07 +0530

    custom routes

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 
 
-const middlewareUser = (req, res, next)=>{
-    req.user = "Guest";
-    next();
-}
+app.get('/orders', (req, res)=>{
+    res.send(`<h1>here is the list of all orders</h1>`);
+})
 
-app.get('/welcome', middlewareUser, (req, res)=>{
-    res.send(`<h1>hello ${req.user}</h1>`);
+app.post('/ordes', (req, res)=>{
+    res.send(`<h1>A new order has been created</h1>`)
+})
+
+app.get('/users', (req, res)=>{
+    res.send(`<h1>here is the list of all users</h1>`)
+})
+app.post('/users', (req, res)=>{
+    res.send(`<h1>a new user has been added</h1>`)
 })
 
 app.listen(3000, ()=>{
