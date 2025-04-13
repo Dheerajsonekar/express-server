ef4000036377210b00836f578a590cb5d061b32f
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 05:30:50 +0530

    unknown routes

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 
+app.use(express.json());
+
+
 app.get('/orders', (req, res)=>{
     res.send(`<h1>here is the list of all orders</h1>`);
 })
 
-app.post('/ordes', (req, res)=>{
+app.post('/orders', (req, res)=>{
     res.send(`<h1>A new order has been created</h1>`)
 })
 
@@ -15,6 +18,10 @@ app.get('/users', (req, res)=>{
 })
 app.post('/users', (req, res)=>{
     res.send(`<h1>a new user has been added</h1>`)
+})
+
+app.use("*", (req, res)=>{
+    res.status(404).send(`<h1> page not found</h1>`)
 })
 
 app.listen(3000, ()=>{
